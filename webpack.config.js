b0e21c1c732f08d24029cf2ca2cb399fee226091
JEROME RENDERS <jeromerenders@gmail.com>
2021-09-24 12:41:26 +0200

    webpack updated gooood

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,32 @@
-const path = require("path");
-const glob = require("glob");
 
-const MiniCssExtractPlugin        = require("mini-css-extract-plugin");
-const LiveReloadPlugin            = require('webpack-livereload-plugin');
-const BrowserSyncPlugin           = require('browser-sync-webpack-plugin');
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+// Utils
+const project = require("./package.json")
+const path    = require("path")
+const glob    = require("glob")
+
+// Plugins
+const MiniCssExtractPlugin        = require("mini-css-extract-plugin")
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+const LiveReloadPlugin            = require('webpack-livereload-plugin')
+const BrowserSyncPlugin           = require('browser-sync-webpack-plugin')
+const UglifyJsPlugin              = require('uglifyjs-webpack-plugin')
+const CssMinimizerPlugin          = require("css-minimizer-webpack-plugin")
+
 
 module.exports = (env, argv) => {
 
     var config = {
 
         mode: "production",
-
+        
         entry: [
-            path.resolve(__dirname, "scripts/builder.coffee"),
+            path.resolve(__dirname, "scripts/builder.coffee"), 
             path.resolve(__dirname, "styles/builder.sass"),
         ].concat(glob.sync('./views/**/[^_]*.pug')),
 
         output: {
-            path: path.resolve(__dirname, "build"),
             filename: "js/bundle.js",
-        },
-        
-        performance: {
-            hints: false,
-            maxEntrypointSize: 512000,
-            maxAssetSize: 512000
+            path: path.resolve(__dirname, "build"),
         },
 
         plugins: [
@@ -35,34 +36,22 @@ module.exports = (env, argv) => {
                 chunkFilename: "[name].css"
             }),
 
-            // new LiveReloadPlugin({
-            //     appendScriptTag: true,
-            //     ignore: /\.js$|\.map$|\.html|\.json$$/
-            // }),
-
-            new BrowserSyncPlugin({
-                port: 3000,
-                host: 'localhost',
-                proxy: "http://localhost/",
-                ignore: [
-                    "**/*.js", "**/*.map", "**/*.html", "**/*.json"
-                ],
-                files: [
-                    {
-                        match: [ "**/*.pug" ],
-                        fn: function(event, file) {
-                            if (event === "change") require("browser-sync").get("bs-webpack-plugin").reload();
-                        }
-                    }
-                ]
-            }, { reload: false }),
-
             new FriendlyErrorsWebpackPlugin(),
 
+            new LiveReloadPlugin({
+                appendScriptTag: true,
+                ignore: /\.js$|\.map$|\.html$/
+            }),
+
+            new BrowserSyncPlugin({
+                proxy: "http://localhost/" + project.name
+            }),
+                   
         ],
 
         module: {
             rules: [
+                // Pug
                 {
                     test: /\.pug$/,
                     use: [
@@ -79,21 +68,26 @@ module.exports = (env, argv) => {
                         } } }
                     ]
                 },
+                
+                // Coffee
                 {
                     test: /\.coffee$/,
                     exclude: /node_modules/,
                     loader: 'coffee-loader',
                 },
+                
+                // Sass
                 {
                     test: /\.sass$/i,
                     exclude: /node_modules/,
                     use: [
                         { loader: MiniCssExtractPlugin.loader },
                         { loader: "css-loader", options: { url: false, sourceMap: true, } },
-                        // { loader: "postcss-loader" },
                         { loader: "sass-loader", options: { sourceMap: true, } }
                     ],
                 },
+                
+                // GLSL
                 {
                     test: /\.(glsl|vs|fs|vert|frag)$/,
                     exclude: /node_modules/,
@@ -104,6 +98,14 @@ module.exports = (env, argv) => {
                 },
             ],
         },
+
+        optimization: {
+            minimizer: [
+                new UglifyJsPlugin(),
+                new CssMinimizerPlugin()
+            ],
+        },
+
     }
 
     return config;
